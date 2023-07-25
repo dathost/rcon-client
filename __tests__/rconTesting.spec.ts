87e32af27e814c9713845b53ca79ce4d6eae6710
Svante Boberg <svante.boberg@gmail.com>
2023-07-25 11:42:35 +0200

    Cleanup tests

--- __tests__/rconTesting.spec.ts
+++ __tests__/rconTesting.spec.ts
@@ -14,23 +14,18 @@ test("Test connection", async () => {
     await client.connect();
     client.disconnect();
   } catch (err) {
-    expect(err).toBe(null);
     console.error(
       'In order to test this you have to run a Rcon server on your local machine on port 25575 with password "testing"!',
     );
+    throw err;
   }
   client.disconnect();
 });
 
 test("Test data fetching", async () => {
   const client = getRconClient();
-  try {
-    await client.connect();
-    const res = await client.send("this command doesn t exist");
-    expect(res).toBe('Unknown command. Type "/help" for help.');
-    client.disconnect();
-  } catch (err) {
-    client.disconnect();
-  }
+  await client.connect();
+  const res = await client.send("this command doesn t exist");
+  expect(res.startsWith("Unknown command")).toBeTruthy();
   client.disconnect();
 });
